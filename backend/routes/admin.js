4c22ae2569ba6ee7f3da8a993fcfbfc3b6e91280
Sumit Shoumya <sumitshoumya15@gmail.com>
2026-02-23 14:24:53 +0530

    fix email port

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -11,7 +11,9 @@ router.use(protect, adminOnly);
 // ── EMAIL HELPER ─────────────────────────────────────────────────
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
